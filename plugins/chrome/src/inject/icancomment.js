9fd1f74fe9e3036ab0f81d9110e9c797dee098b4
Chris Larsen <cl.arsen.net+github@gmail.com>
2015-10-10 13:56:17 -0500

    added url= to query string

--- plugins/chrome/src/inject/icancomment.js
+++ plugins/chrome/src/inject/icancomment.js
@@ -4,11 +4,11 @@ $( function(){
 	}else{
 		var currentURL = document.location.href
 	}
-	$( "body" ).prepend( '<div id="icancomment"><button>&#128172;</button><iframe src="http://localhost:3000?"' + encodeURIComponent( currentURL ) + '></iframe></div>' );
+	$( "body" ).prepend( '<div id="icancomment"><button>&#128172;</button><iframe src="http://localhost:3000?url="' + encodeURIComponent( currentURL ) + '></iframe></div>' );
 	$('#icancomment').click( function(){
 		
 		
-		var theHeight = $('#icancomment').height() == '50' ? '80%' : 50;
+		var theHeight = $('#icancomment').height() == '30' ? '80%' : 30;
 		var theWidth = $('#icancomment').width() == '30' ? '100%' : 30;
 		$('#icancomment').height( theHeight );
 		$('#icancomment').width( theWidth );
