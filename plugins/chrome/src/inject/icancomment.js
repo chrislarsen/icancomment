61fe4ed894b7fbbfdfb8b7a3deaa140fe78067c5
Chris Larsen <cl.arsen.net+github@gmail.com>
2015-10-10 13:31:52 -0500

    toggle width to ensure that no content is blocked when the comments are closed

--- plugins/chrome/src/inject/icancomment.js
+++ plugins/chrome/src/inject/icancomment.js
@@ -4,12 +4,14 @@ $( function(){
 	}else{
 		var currentURL = document.location.href
 	}
-	$( "body" ).append( '<div id="icancomment"><button>&#128172;</button><iframe src="http://localhost:3000?"' + encodeURIComponent( currentURL ) + '></iframe></div>' );
+	$( "body" ).prepend( '<div id="icancomment"><button>&#128172;</button><iframe src="http://localhost:3000?"' + encodeURIComponent( currentURL ) + '></iframe></div>' );
 	$('#icancomment').click( function(){
 		
 		
 		var theHeight = $('#icancomment').height() == '50' ? '80%' : 50;
+		var theWidth = $('#icancomment').width() == '30' ? '100%' : 30;
 		$('#icancomment').height( theHeight );
+		$('#icancomment').width( theWidth );
 		$('#icancomment iframe').toggle();
 	} )
 })
