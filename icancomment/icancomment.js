16b8088d99417f8cd674a60b75456458801c10c3
Kumar Unnikrishnan <ksunair@yahoo.com>
2015-10-11 11:25:48 -0500

    added logic for intro page

--- icancomment/icancomment.js
+++ icancomment/icancomment.js
@@ -5,34 +5,22 @@ if (Meteor.isClient) {
     template: 'bootstrap'
   });
 
-  Template.hello.commandId = function () {
-    return Router.current().params.query.url;
-  };
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
 
   Template.main.commandId = function () {
     return Router.current().params.query.url;
   };
 
-  Router.map(function () {
-    this.route('home', {
-      path: '/',
-      template: 'main',
-      data: function () {
-        // the data function is an example where this.params is available
-
-        // we can access params using this.params
-        // see the below paths that would match this route
-       commandGroupID: this.params;
-
-        // we can access query string params using this.params.query
-        var queryStringParams = this.params.query;
-
-        // query params are added to the 'query' object on this.params.
-        // given a browser path of: '/?task_name=abcd1234
-        // this.params.query.task_name => 'abcd1234'
-      }
-    });
+  Router.route('/', function () {
+    if( !this.params.query.url ){
+      this.render( 'intro' );
+    }else{
+      this.render( "main" );
+    }
   });
+
 }
 
 if (Meteor.isServer) {
